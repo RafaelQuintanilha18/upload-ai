b1f005b8d8e68b78c6f125e9ddfc093167e79a14
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2023-10-04 19:01:41 +0100

    ..

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -6,8 +6,6 @@ import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
-const port = process.env.PORT || 3333;
-
 
 app.register(fastifyCors, {
   origin: '*',
@@ -19,12 +17,8 @@ app.register(createTranscriptionRoute)
 app.register(generateAICompletionRoute)
 
 
-/* app.listen({
+app.listen({
   port: 3333,
 }).then(() => {
   console.log('HTTP Server Running!')
-}) */
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-});
+})

9035e43b167c0e220b820d0adbf72a1a63f5b81d
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2023-10-04 19:24:26 +0100

    ....

--- src/lib/axious.ts
+++ src/lib/axious.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 
-export const api = axios.create({
+/* export const api = axios.create({
   baseURL: 'http://localhost:3333',
-})
+}) */
+
+export const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
+});

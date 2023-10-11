31180adc4461c42c54a740e70de2273583562067
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2023-10-11 16:42:18 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite"
 import path from "path"
 import react from "@vitejs/plugin-react"
+import dotenvExpand from 'dotenv-expand';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // This check is important!
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '');
+    dotenvExpand({ parsed: env });
+  }
+
+  return {
+    plugins: [react()],
+    optimizeDeps: {
+      exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
     },
-  },
-})
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
